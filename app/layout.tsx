8b66825c8a45310ece035850dddccbef45dcd2ef
alexbvart <alexanderbvart@gmail.com>
2023-06-13 10:33:09 -0500

    init navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import Link from 'next/link'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -17,7 +18,14 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className='flex'>
-          <div className='h-screen w-32 bgCard'> logo </div>
+          <div className='h-screen w-32 bgCard'> 
+            <ul>
+              <li> <Link href="/">Logo</Link> </li>
+              <li> <Link href="task">Task</Link> </li>
+              <li> <Link href="board">Board</Link> </li>
+              <li> <Link href="calendar">TimeSheet</Link> </li>
+            </ul>
+          </div>
           <div className="w-full">
             {children}
           </div>
